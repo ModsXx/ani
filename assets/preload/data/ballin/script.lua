a54b43749bd3042a8523720616c4239969484df8
ModsXx <118561210+ModsXx@users.noreply.github.com>
2023-03-04 01:07:32 -0500

    Add files via upload

---
+++ assets/preload/data/ballin/script.lua
@@ -0,0 +1,7 @@
+local allowCountdown = false
+function opponentNoteHit()
+    health = getProperty('health')
+    if getProperty('health') > 0.1 then
+        setProperty('health', health- 0.01);
+    end
+end
